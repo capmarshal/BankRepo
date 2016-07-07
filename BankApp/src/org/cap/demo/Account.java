1a7644d6d37b794d5e61c85af3b3c9cfecbf3222
Vagish <vagish.gupta@capgemini.com>
2016-07-07 14:21:44 +0530

    Initial Commit

---
+++ BankApp/src/org/cap/demo/Account.java
@@ -0,0 +1,10 @@
+package org.cap.demo;
+
+public class Account {
+	
+	
+	public void deposit(){
+		System.out.println("DEposit Operation");
+	}
+
+}
---
+++ BankApp/src/org/cap/demo/MainClass.java
@@ -0,0 +1,19 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Account account = new Account();
+		
+		account.deposit();
+		
+	}
+	
+	public static void greetings(){
+		
+		System.out.println("hello");
+	} 
+	
+
+}
